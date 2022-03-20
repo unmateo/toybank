4cb10b3b6c29baa7a53317e2e3ac606a6c279057
Mateo Harfuch <mateo.harfuch@onapsis.com>
2022-03-20 11:44:42 -0300

    test: one customer multiple accounts

--- tests/api/test_customer_account.py
+++ tests/api/test_customer_account.py
@@ -1,3 +1,6 @@
+from urllib import response
+
+
 def test_new_customer_has_no_accounts(client, given_customer):
     response = client.get(f"/customers/{given_customer().id}/accounts")
     assert response.status_code == 200
@@ -29,3 +32,15 @@ def test_create_customer_account_with_initial_balance(client, given_customer):
     account = response.json()
     assert response.status_code == 201
     assert account["balance"] == 50
+
+
+def test_customer_multiple_accounts(client, given_customer):
+    account_a = {"alias": "Savings Account"}
+    account_b = {"alias": "Retirement Account"}
+    path = f"/customers/{given_customer().id}/accounts"
+    client.post(path, json=account_a)
+    client.post(path, json=account_b)
+    response = client.get(path)
+    accounts = response.json()
+    assert accounts[0]["alias"] == account_a["alias"]
+    assert accounts[1]["alias"] == account_b["alias"]
